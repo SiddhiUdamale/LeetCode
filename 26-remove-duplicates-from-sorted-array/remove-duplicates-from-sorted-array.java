a9599a9ca1af368b5fea29b7a870e8e8a7cd4cda
SiddhiUdamale <169069686+SiddhiUdamale@users.noreply.github.com>
2025-10-06 16:18:43 +0530

    Time: 391 ms (5.14%) | Memory: 44.6 MB (96.79%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,22 +1,22 @@
-// class Solution {
-//     public int removeDuplicates(int[] nums) {
-//        boolean flag = true;
-//        int count = 0;
-//       for(int i=0;i<nums.length;i++){
-//           flag = true;
-//           for(int j=0;j<i;j++){
-//                if(i==0){
-//               flag = true;
-//           }else{
-//               if(nums[i] == nums[j]){
-//                   flag = false;
-//               }
-//           }
-//           }
-         
-//           if(flag){
-//               count++;
-//           }   
+class Solution {
+    public int removeDuplicates(int[] nums) {
+      
+       int count = 0;
+       int a=0;
+       int n[] = new int[nums.length];
+      for(int i=0;i<nums.length;i++){
+           boolean flag = true;
+          for(int j=i-1;j>=0;j--){
+            if(nums[i]==nums[j])flag =false;
+          }
+          if(flag){
+            nums[a]= nums[i];
+            a++;
+          }  
+      }
+      return a;
+      }
+    } 
 //       }
 // int n[] = new int[nums.length];
 //       flag = true;
@@ -38,24 +38,21 @@
 //               ind++;
 //           }   
 //       }
-//       return n;
+//       return ind;
 //     }
 // }
 
 
-class Solution {
-    public int removeDuplicates(int[] nums) {
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
 
-        // int arr[]=int new[nums.length];
-
-        // for(int i=0;i<)
-
-        int a=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]==nums[i])continue;
-            nums[a]=nums[i];
-            a++;
-        }
-        return a;
-    }
-}
+       
+//         int a=1;
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i-1]==nums[i])continue;
+//             nums[a]=nums[i];
+//             a++;
+//         }
+//         return a;
+//     }
+// }
